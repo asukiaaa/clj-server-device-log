14ee945aaa33369bc73c125a2c87733d172dc16f
Asuki Kono <asukiaaa@gmail.com>
2022-02-24 21:25:15 +0900

    Initial commit after clj-new

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to clj-server-practice
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/asuki/clj_server_practice_test.clj
@@ -0,0 +1,7 @@
+(ns asuki.clj-server-practice-test
+  (:require [clojure.test :refer :all]
+            [asuki.clj-server-practice :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
