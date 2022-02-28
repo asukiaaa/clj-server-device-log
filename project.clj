590f1e8c7086cceca582db49ced3152ace5d94ae
Asuki Kono <asukiaaa@gmail.com>
2022-02-28 16:24:47 +0900

    Define dependencies and define clojure

--- project.clj
+++ project.clj
@@ -1,3 +1,3 @@
 (defproject clj-server-practice "0.1.0-SNAPSHOT"
   :description "A project to practice creating server in clj."
-  :plugins [[lein-heroku "0.5.3"]])
+  :dependencies [[org.clojure/clojure "1.10.3"]])
