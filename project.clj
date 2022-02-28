fad7c1a2a43c510394a84c186bab4382bb50b7f6
Asuki Kono <asukiaaa@gmail.com>
2022-02-28 12:53:11 +0900

    Add lein-heroku as plugin on project.clj

--- project.clj
+++ project.clj
@@ -1,2 +1,3 @@
 (defproject clj-server-practice "0.1.0-SNAPSHOT"
-  :description "A project to practice creating server in clj.")
+  :description "A project to practice creating server in clj."
+  :plugins [[lein-heroku "0.5.3"]])
