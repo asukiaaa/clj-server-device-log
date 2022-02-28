e94f0027b907f0fad23da8ea01c133b7d6a2270a
Asuki Kono <asukiaaa@gmail.com>
2022-02-28 16:37:51 +0900

    Add min lein-version to project.clj

--- project.clj
+++ project.clj
@@ -1,3 +1,4 @@
 (defproject clj-server-practice "0.1.0-SNAPSHOT"
   :description "A project to practice creating server in clj."
+  :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.10.3"]])
