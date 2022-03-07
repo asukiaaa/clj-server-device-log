99bb027cd46bd4487fd593523cc629f613df6b90
Asuki Kono <asukiaaa@gmail.com>
2022-03-07 20:36:54 +0900

    Remove clojure from dependencies in project.clj

--- project.clj
+++ project.clj
@@ -1,4 +1,3 @@
 (defproject clj-server-practice "0.1.0-SNAPSHOT"
   :description "A project to practice creating server in clj."
-  :min-lein-version "2.0.0"
-  :dependencies [[org.clojure/clojure "1.10.3"]])
+  :min-lein-version "2.0.0")
