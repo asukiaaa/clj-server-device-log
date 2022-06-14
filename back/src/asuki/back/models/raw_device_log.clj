c83748c705d59ce118e96c726cd09c194db51079
Asuki Kono <asukiaaa@gmail.com>
2022-06-14 19:07:17 +0900

    Support max group_by

--- back/src/asuki/back/models/raw_device_log.clj
+++ back/src/asuki/back/models/raw_device_log.clj
@@ -3,6 +3,7 @@
             [clojure.string :refer [join escape]]
             [clojure.core :refer [re-find re-matcher]]
             [clojure.data.json :as json]
+            [clojure.walk :as walk]
             [clj-time.core :as t]
             [clj-time.format :as f]
             [asuki.back.config :refer [db-spec]]))
@@ -24,11 +25,12 @@
   (when (.contains ["DESC" "desc" "ASC" "asc"] dir)
     dir))
 
-(defn build-target-key [{:keys [key table-key]}]
-  #_(println "build-target-key" key table-key)
-  (let [record-key (if (string? key) key (first key))
+(defn build-target-key [{:keys [key table-key] :as args}]
+  #_(println "build-target-key" key table-key args)
+  (let [fn-filter-key (or (:filter-key args) filter-key)
+        record-key (if (string? key) key (first key))
         json-key (when-not (string? key) (rest key))
-        escaped-key (filter-key record-key)]
+        escaped-key (fn-filter-key record-key)]
     (when-let [key-with-table (when-not (nil? escaped-key)
                                 (str (when table-key (str table-key ".")) escaped-key))]
       (if (empty? json-key)
@@ -101,14 +103,68 @@
                             (t/minus (t/now) (t/hours (Integer. str-hours-from-action))))])
       :else (join " " [target-key target-action target-value]))))
 
+(defn build-keys-for-where-max-group-by [base-table-key index]
+  #_(println "build-keys-for-where-max-group-by" base-table-key index)
+  {:table (str base-table-key "_where_max_group_" index)
+   :max (str "val_max_" index)
+   :group-by (str "val_group_by_" index)})
+
+(defn build-query-item-where-max-group-by [index item {:keys [base-table-key]}]
+  #_(println "build-query-item-where-max-group-by" index item base-table-key)
+  (let [keys (build-keys-for-where-max-group-by base-table-key index)
+        max-group-by-table-key (:table keys)
+        target-key-max (build-target-key {:key (:max keys)
+                                          :table-key max-group-by-table-key
+                                          :filter-key (fn [x] x)})
+        base-key-max (build-target-key (walk/keywordize-keys (get item "max")))
+        target-key-group-by (build-target-key {:key (:group-by keys)
+                                               :table-key max-group-by-table-key
+                                               :filter-key (fn [x] x)})
+        base-key-group-by (build-target-key (walk/keywordize-keys (get item "group_by")))]
+    #_(println target-key-max base-key-max target-key-group-by base-key-group-by)
+    (join " " [target-key-max "=" base-key-max "AND" target-key-group-by "=" base-key-group-by])))
+
+(defn where-max-group-by? [item]
+  (or (get item "group_by") (get item "max")))
+
 (defn build-query-where [{:keys [where db-table-key base-table-key]}]
   (when-not (or (nil? where) (empty? where))
     #_(println "where" where)
-    (str "WHERE "
-         (join " AND " (for [item where]
-                         #_(println "item" item)
-                         (build-query-item-where item {:db-table-key db-table-key
-                                                       :base-table-key base-table-key}))))))
+    (let [where-max-group-by (filter where-max-group-by? where)
+          where-normal (filter #(not (where-max-group-by? %)) where)]
+      (str "WHERE "
+           (join " AND "
+                 (filter #(not (empty? %))
+                         [(join " AND "
+                                (for [[index item] (map-indexed vector where-max-group-by)]
+                                  #_(println "item" item)
+                                  (build-query-item-where-max-group-by index item {:base-table-key base-table-key})))
+                          (join " AND "
+                                (for [item where-normal]
+                                  #_(println "item" item)
+                                  (build-query-item-where item {:db-table-key db-table-key
+                                                                :base-table-key base-table-key})))]))))))
+
+(defn build-query-select-max-group-by [where-max-group-by {:keys [db-table-key base-table-key]}]
+  #_(println "build-query-select-max-group-by" where-max-group-by)
+  (when-not (empty? where-max-group-by)
+    (let [;; table-keys (for [item where-max-group-by] (str ))
+          query (join ", "
+                      (for [[index item] (map-indexed vector where-max-group-by)]
+                        (let [keys-max-group-by (build-keys-for-where-max-group-by base-table-key index)
+                              table-key (:table keys-max-group-by)
+                              converted-max-key (:max keys-max-group-by)
+                              converted-group-by-key (:group-by keys-max-group-by)
+                              group-by-key (build-target-key (walk/keywordize-keys (get item "group_by")))
+                              query (join " " ["(SELECT"
+                                               "max(" (build-target-key (walk/keywordize-keys (get item "max"))) ") AS" converted-max-key ","
+                                               group-by-key "AS" converted-group-by-key
+                                               "FROM" db-table-key
+                                               "GROUP BY" group-by-key
+                                               ") as" table-key])]
+                          query)))]
+      (println "query for select-max-group-by" query)
+      query)))
 
 (defn get-records-with-total [& [args]]
   (println "get-records-with-total" args)
@@ -119,12 +175,18 @@
         where (json/read-str (:where args))
         db-table-key "raw_device_log"
         base-table-key "rdl"
-        str-query (join " " ["select SQL_CALC_FOUND_ROWS * from" db-table-key "as" base-table-key
+        where-max-group-by (filter where-max-group-by? where)
+        str-query-select-max-group-by
+        (build-query-select-max-group-by where-max-group-by
+                                         {:db-table-key db-table-key
+                                          :base-table-key base-table-key})
+        str-query (join " " ["SELECT SQL_CALC_FOUND_ROWS * FROM" db-table-key "AS" base-table-key
+                             (when-not (empty? str-query-select-max-group-by) (str ", " str-query-select-max-group-by))
                              (build-query-where {:where where
                                                  :db-table-key db-table-key
                                                  :base-table-key base-table-key})
                              (build-query-order order base-table-key)
-                             "limit 0," limit])]
+                             "LIMIT 0," limit])]
     (println "str-query " str-query)
     (jdbc/with-db-transaction [db-transaction db-spec]
       {:records (jdbc/query db-transaction str-query)
