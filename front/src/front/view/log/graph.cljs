e0fa23e0ffb1342b6c142c20cba3ba6030f4a7d8
Asuki Kono <asukiaaa@gmail.com>
2024-02-29 15:41:47 +0900

    Show line chart by time scale

--- front/src/front/view/log/graph.cljs
+++ front/src/front/view/log/graph.cljs
@@ -1,7 +1,43 @@
 (ns front.view.log.graph
-  (:require [cljsjs.chartjs]
-            ["react" :as react]
-            [front.model.raw-device-log :as model.log]))
+  (:require
+   [cljsjs.chartjs]
+   [cljsjs.chartjs-adapter-date-fns]
+   [cljsjs.date-fns]
+   #_"chart.js"
+   #_["chart.js" :as chart]
+   #_["chart.js" :refer (Chart)]
+   #_"moment"
+   #_"chartjs-adapter-moment"
+   #_["chartjs-adapter-moment" :as adapter]
+   #_["chart.js/auto" :refer [Chart]]
+   ["react" :as react]
+   #_["date-fns/locale/index.js" :refer [ja]]
+   [front.model.raw-device-log :as model.log]))
+
+#_(js/require "chart.js")
+#_(js/require "moment")
+#_(js/require "chartjs-adapter-moment")
+
+#_(def chart (js/require "chart.js/auto"))
+#_(js/require "chartjs-adapter-date-fns")
+#_(.registerer chart/Chart (chart/registerers))
+#_(println (chart/Chart.register (... chart/registerables)))
+#_(println (chart/registerables js/...))
+#_(doseq [r chart/registerables] #_(println r) (.register chart/Chart r))
+#_(.register chart/Chart chart/TimeScale)
+#_(.log js/console adapter)
+#_(println chart/registerables #_(first))
+
+#_(.log js/console "adapters")
+#_(println chart/_adapters)
+#_(.log js/console (.-_date chart/_adapters))
+
+#_(def  values [{:x "2020-02-01" :y 100}
+              {:x "2020-02-02" :y 200}
+              {:x "2020-02-03" :y 300}
+              {:x "2020-02-04" :y 400}
+              {:x "2020-02-07" :y 200}
+              {:x "2020-02-14" :y 600}])
 
 (defn render-graph-canvas [config graph-id]
   (react/useEffect
@@ -14,11 +50,28 @@
 
 (defn render-graph [logs val-config]
   (let [val-key (get val-config "key")
-        config {:type "line"
-                :data {:labels (for [log logs] (str (model.log/get-val-from-record log "created_at")))
-                       :datasets [{:data (for [log logs] (model.log/get-val-from-record log val-key))
-                                   :label (model.log/get-label-from-col-config val-config)
-                                   :backgroundColor "#90EE90"}]}}]
+        config #_{:type "line"
+                  :data {:datasets [{:data values
+                                     :backgroundColor "#90EE90"}]}
+                  :options
+                  {:scales
+                   {:x
+                    {:type "time"
+                     :time {:unit "day"
+                            :displayFormats {:week "yyyy-MM-dd"}}}}}}
+        {:type "line"
+         :data {#_:labels #_[] #_(for [log logs] (str (model.log/get-val-from-record log "created_at")))
+                :datasets [{:data (for [log logs] #_(model.log/get-val-from-record log val-key)
+                                       {:x #_"2024-120-20" (:created_at log)
+                                        :y (model.log/get-val-from-record log val-key)})
+                            #_:label #_(model.log/get-label-from-col-config val-config)
+                            :backgroundColor "#90EE90"}]}
+         :options
+         {:scales
+          {:x
+           {:type "time"
+            :time {:displayFormats {:week "MM/dd"
+                                    :day "MM/dd"}}}}}}]
     [:f> render-graph-canvas config (str "graph-" val-key)]))
 
 (defn core [str-where str-order config-renderer config-renderer-graph]
@@ -29,7 +82,6 @@
        (model.log/fetch-list {:where str-where :order str-order :on-receive on-receive})
        (fn []))
      #js [str-where str-order])
-
     [:<>
      (when-not (empty? logs)
        (for [val-config config-renderer]
