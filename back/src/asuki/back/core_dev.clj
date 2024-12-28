46a92e7e7571e2d6c2036ed97e69d28e71fb67e9
Asuki Kono <asukiaaa@gmail.com>
2024-12-28 09:36:31 +0900

    Remove not used functions from core_dev

--- back/src/asuki/back/core_dev.clj
+++ back/src/asuki/back/core_dev.clj
@@ -1,17 +1,8 @@
 (ns asuki.back.core-dev
   (:require [asuki.back.core :as core]
-            [nrepl.server]
-            [clojure.tools.namespace.repl :refer [refresh]]))
+            [nrepl.server]))
 
 (defn run-dev-servers [& _args]
   (nrepl.server/start-server :port 59595
                              :bind "0.0.0.0")
   (core/run-relodable))
-
-(defn db-migrate-with-reloading []
-  #_(refresh)
-  (core/db-migrate))
-
-(defn db-rollback-with-reloading []
-  #_(refresh)
-  (core/db-rollback))
