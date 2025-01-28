518d56ca46f6ea4e331a18fb6937967906e708e3
Asuki Kono <asukiaaa@gmail.com>
2025-01-28 19:55:50 +0900

    Apply file.page for dashoard

--- front/src/front/view/dashboard.cljs
+++ front/src/front/view/dashboard.cljs
@@ -1,40 +1,18 @@
 (ns front.view.dashboard
-  (:require ["react" :as react]
-            ["react-bootstrap" :as bs]
+  (:require ["react-bootstrap" :as bs]
             ["react-router-dom" :as router]
             [front.model.device-file :as model.device-file]
             [front.model.user :as model.user]
             [front.route :as route]
             [front.view.common.wrapper.show404 :as wrapper.show404]
-            [front.view.common.wrapper.fetching :as wrapper.fetching]
             [front.view.util.breadcrumb :as breadcrumb]
-            [front.view.util.device-file.card :as file.card]
             [front.view.util.label :as util.label]
-            [front.view.util :as util]))
+            [front.view.util :as util]
+            [front.view.util.device-file.page :as file.page]))
 
 (defn page []
   (let [user-loggedin (util/get-user-loggedin)
-        is-admin (model.user/admin? user-loggedin)
-        location (router/useLocation)
-        info-wrapper-fetching (wrapper.fetching/build-info #(react/useState))
-        query-params (util/read-query-params)
-        number-page (or (:page query-params) 0)
-        number-limit (or (:limit query-params) 30)
-        [files-list-and-total set-files-list-and-total] (react/useState)
-        load-list
-        (fn []
-          (wrapper.fetching/start info-wrapper-fetching)
-          (model.device-file/fetch-list-and-total-latest-each-device
-           {:limit number-limit
-            :page number-page
-            :on-receive (fn [result errors]
-                          (set-files-list-and-total result)
-                          (wrapper.fetching/finished info-wrapper-fetching errors))}))]
-    (react/useEffect
-     (fn []
-       (load-list)
-       (fn []))
-     #js [location])
+        is-admin (model.user/admin? user-loggedin)]
     [:<>
      [:f> breadcrumb/core []]
      [:> bs/Container {:fluid true}
@@ -50,16 +28,7 @@
            [:<> {:key name}
             [:> router/Link {:to url :class "list-group-item list-group-item-action"} name]])]]
        [:> bs/Col {:class :px-0}
-        (wrapper.fetching/wrapper
-         {:info info-wrapper-fetching
-          :renderer
-          [:<>
-           (let [files (:list files-list-and-total)]
-             (if (empty? files)
-               [:div util.label/no-file-to-show]
-               (for [file files]
-                 [:<> {:key (:path file)}
-                  [:f> file.card/core file]])))]})]]]]))
+        [:f> file.page/core model.device-file/fetch-list-and-total-latest-each-device]]]]]))
 
 (defn core []
   (wrapper.show404/wrapper
